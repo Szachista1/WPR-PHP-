93622ed6fac06cd7ea8a9ade227bf05d3d1b4783
DESKTOP-TG98GK0\igwar <s26986@edu.pjwstk.pl>
2023-03-15 16:22:27 +0100

    zad 1.2 1.4 1.1 zrobione

---
+++ zadania1/z.1.4.php
@@ -0,0 +1,12 @@
+<?php
+$pesel=191203526;
+function urodziny($pesel)
+{
+    global $pesel;
+    $rok=substr($pesel,0,2);
+    $dzien=substr($pesel,4,2);
+    $miesiac=substr($pesel,2,2);
+    return $dzien . "-" . $miesiac ."-" . $rok;
+}
+echo urodziny($pesel);
+?>
