5760bad4e9448f2429c0fb411f00bbc644a296f4
DESKTOP-TG98GK0\igwar <s26986@edu.pjwstk.pl>
2023-04-26 18:54:08 +0200

    cos zrobione

--- zadania5/zad3.php
+++ zadania5/zad3.php
@@ -1,27 +1,10 @@
 <?php
-$dni=365;
 
-if(!isSet($_COOKIE['hits'])){
-    $hits=1;}
-else
-{$hits= intval($_COOKIE['hits']);
-    $hits++;
+setcookie("mycookie", time(), time()+3600);
+if(isset($_COOKIE["mycookie"])) {
+    echo "Wartość ciasteczka to: ".$_COOKIE["mycookie"];
+} else {
+    echo "Ciasteczko nie istnieje.";
 }
-setCookie("hits","$hits",time()+60*60*24*$dni);
-echo("Ta strona została odwiedzona $hits razy.");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-    <meta http-equiv="Content-type" content="text/html; charset=iso-8859-2">
-    <meta name="Description" content=" [wstaw tu opis strony] ">
-    <meta name="Keywords" content=" [wstaw tu slowa kluczowe] ">
-    <meta name="Author" content=" [dane autora] ">
-    <meta name="Generator" content="kED2">
 
-    <title> [tytuł strony] </title>
-
-    <link rel="stylesheet" href=" [nazwa_arkusza_stylow.css] " type="text/css">
-</head>
-
-</html>
---
+++ zadania6/dodaj_samochody.php
@@ -0,0 +1,31 @@
+<HTML>
+<FORM name="form" action="dodaj_samochody.php" method="post">
+Dodaj samochód <BR>
+marka: <input type="text", name="marka", maxlength="40"> <BR>
+model: <input type="text", name="model", maxlength="40"> <BR>
+rok: <input type="text", name="rok", maxlength="4"><BR>
+opis: <input type="text", name="opis", maxlength="400"><BR>
+cena: <input type="text", name="cena"><BR>
+<input type="submit", value="wyślij">
+    <?php
+
+    if ($_SERVER['REQUEST_METHOD']=='POST') {
+    if(isset($_POST['marka']) && isset($_POST['model']) && isset($_POST['rok']) && isset($_POST['opis']) && isset($_POST['cena'])) {
+    if(!empty($_POST['marka']) && !empty($_POST['model']) && !empty($_POST['marka']) && is_numeric($_POST['rok']) && !empty($_POST['opis']) && is_numeric($_POST['cena'])) {
+    if (!$x=mysqli_connect("localhost", "root" , "", "mojaBaza" ) )   {
+    echo "Nie udało się zrbić";
+    exit;
+    $query="INSERT INTO samochody  (marka, model, rok, opis, cena) ";
+$query.="VALUES ('{$_POST['marka']}', '{$_POST['model']}', {$_POST['rok']}, '{$_POST['opis']}', {$_POST['cena']})";
+$rezultat=mysqli_query($x, $query);
+if (mysqli_affected_rows($x)==1) { echo "został dodany";
+}
+if (!mysqli_close($x)){
+    echo "jest prblem";
+    exit();
+}
+    }
+    }   }}
+    ?>
+</FORM>
+</HTML>
