f3fbc69699c1b7113de4c10c137728a818b4252d
DESKTOP-TG98GK0\igwar <s26986@edu.pjwstk.pl>
2023-05-10 17:32:44 +0200

    cos zrobione

--- projekt/forum.php
+++ projekt/forum.php
@@ -1,5 +1,8 @@
 <?php
-session_start()
+session_start();
+if(isset($_POST['zalogowany']) && isset($_POST['login']) && isset($_POST['hasło']))
+
+
 ?>
 <HTML>
 <head>
@@ -9,6 +12,8 @@ session_start()
 </title>
 </head>
 <form method="POST" action="forum.php">
-
+Login(max 13):<input type="text" maxlength="13" name="login"><br>
+Hasło:<input type="password" name="hasło"><br>
+<input type="submit" name="zalogowany" value="użytkownik_zalogowany">
 </body>
 </HTML>
