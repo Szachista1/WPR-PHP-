9b9d7232aa738944aae266b1c9911af9367252ee
DESKTOP-TG98GK0\igwar <s26986@edu.pjwstk.pl>
2023-05-21 18:22:55 +0200

    z

--- projkt/zad8.1.php
+++ projkt/zad8.1.php
@@ -1,11 +1,18 @@
 <?php
+date_default_timezone_set('GMT');
+$data=date_default_timezone_get();
 session_start();
 ?>
 <HTML>
 <title>Forum internetowe</title>
 <BODY>
 <form action="zad8.2.php" method="post">
-
+    Jęśli nie masz i nie masz u nas konta i nie chcesz mieć to wypełnij te dane:<br>
+Temat:    <input type="text" name="temat"><br>
+Wpis:    <input type="text" name="wpis"><br>
+Data: <?php echo $data;
+    ?><br>
+Psełdonim:<input type="text" name="psełdonim">
     Witamy na forum internetowym.<br>
     Jeśli masz u nas konto to podaj e-mail oraz hasło.
     <input type="email" name="e-mail"><br>
