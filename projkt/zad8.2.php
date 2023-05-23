96e1e8bef91f892bea41cfc333e765d0141819b3
DESKTOP-TG98GK0\igwar <s26986@edu.pjwstk.pl>
2023-05-23 13:10:19 +0200

    z

--- projkt/zad8.2.php
+++ projkt/zad8.2.php
@@ -2,16 +2,18 @@
 <?php
 session_start();
 if (!empty($_POST['e-mail']) || !empty($_POST['hasło'])){
-if (isset($_POST['e-mail']) || isset($_POST['hasło'])){
-
-}}
-elseif (isset($_POST['temat'])) echo $_POST['temat'];
-?>
-
-<html>
+    if (isset($_POST['e-mail']) || isset($_POST['hasło'])) ?>{
+    <html>
 <form method="post" action="zad8.3.php">
-Wpis:    <input type="text" name="wpis">
-Temat:    <input type="text" name="temat">
+        Wpis:    <input type="text" name="wpis">
+        Temat:    <input type="text" name="temat">
     <input type="submit" value="Wyślij" name="dział">
 </form>
+
+<?php
+}
+elseif (isset($_POST['temat'])) {
+    echo $_POST['temat']; }
+?>
+
 </html>
