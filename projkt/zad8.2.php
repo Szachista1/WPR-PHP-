e088362473c82d40f9ee5e803190e7c819ad35c9
DESKTOP-TG98GK0\igwar <s26986@edu.pjwstk.pl>
2023-05-27 17:46:29 +0200

    lol

--- projkt/zad8.2.php
+++ projkt/zad8.2.php
@@ -9,7 +9,7 @@ if (!empty($_POST['e-mail']) || !empty($_POST['hasło'])){
         Temat:    <input type="text" name="temat">
     <input type="submit" value="Wyślij" name="dział">
 </form>
-
+    </html>
 <?php
 }
    else if (!empty($_POST['wpis']) || !empty($_POST['psełdonim'])) {
@@ -40,46 +40,52 @@ if (!empty($_POST['e-mail']) || !empty($_POST['hasło'])){
            }
 
            // Zamknięcie połączenia z bazą danych
-           $mysqli->close();
+           $mysqli->close(); } ?>
+            <HTML>
+            <form method="post" action="zad8.3.php">
+            Wpis:<input type="text" name="wpis">
+            Psełdonim:<input type="text" name="psełdonim">
+            </form>
+            </HTML>
 
+
+
+<?php
        }
-
-   }
 elseif(!empty($_POST['e-mailn'])|| !empty($_POST['psełdonimn']) || !empty($_POST['hasłon'])) {
-if(isset($_POST['e-mailn']) || isset($_POST['psełdonimn']) || isset($_POST['hasłon']) || isset($_POST['profilowen'])) {
+    if (isset($_POST['e-mailn']) || isset($_POST['psełdonimn']) || isset($_POST['hasłon']) || isset($_POST['profilowen'])) {
 
-    $target_directory = "C:\Users\igwar\WPR-PHP-\projkt\profilowe"; // Zmień na właściwą ścieżkę docelową
-    $target_file = $target_directory . basename($_FILES["profilawen"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        $target_directory = "C:\Users\igwar\WPR-PHP-\projkt\profilowe"; // Zmień na właściwą ścieżkę docelową
+        $target_file = $target_directory . basename($_FILES["profilawen"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    // Sprawdzenie czy plik jest faktycznie przesłany
-    if (isset($_FILES["profilawen"])) {
-        // Sprawdzenie czy plik już istnieje
-        if (file_exists($target_file)) {
-            echo "Przepraszamy, plik o tej nazwie już istnieje.";
-            $uploadOk = 0;
-        }
-        // Sprawdzenie rozmiaru pliku (opcjonalne)
-        if ($_FILES["profilawen"]["size"] > 500000) { // Zmień na odpowiedni limit rozmiaru
-            echo "Przepraszamy, twój plik jest zbyt duży.";
-            $uploadOk = 0;
-        }
-        // Sprawdzenie typu pliku (opcjonalne)
-        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif") { // Zmień na odpowiednie rozszerzenia plików
-            echo "Przepraszamy, akceptujemy tylko pliki JPG, JPEG, PNG i GIF.";
-            $uploadOk = 0;
-        }
-        // Jeżeli nie ma żadnych błędów, przenieś plik do docelowej ścieżki
-        if ($uploadOk == 1) {
-            if (move_uploaded_file($_FILES["profilawen"]["tmp_name"], $target_file)) {
-                echo "Plik został pomyślnie przesłany i zapisany w: " . $target_file;
-            } else {
-                echo "Wystąpił problem podczas przesyłania pliku.";
+        // Sprawdzenie czy plik jest faktycznie przesłany
+        if (isset($_FILES["profilawen"])) {
+            // Sprawdzenie czy plik już istnieje
+            if (file_exists($target_file)) {
+                echo "Przepraszamy, plik o tej nazwie już istnieje.";
+                $uploadOk = 0;
+            }
+            // Sprawdzenie rozmiaru pliku (opcjonalne)
+            if ($_FILES["profilawen"]["size"] > 500000) { // Zmień na odpowiedni limit rozmiaru
+                echo "Przepraszamy, twój plik jest zbyt duży.";
+                $uploadOk = 0;
+            }
+            // Sprawdzenie typu pliku (opcjonalne)
+            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif") { // Zmień na odpowiednie rozszerzenia plików
+                echo "Przepraszamy, akceptujemy tylko pliki JPG, JPEG, PNG i GIF.";
+                $uploadOk = 0;
+            }
+            // Jeżeli nie ma żadnych błędów, przenieś plik do docelowej ścieżki
+            if ($uploadOk == 1) {
+                if (move_uploaded_file($_FILES["profilawen"]["tmp_name"], $target_file)) {
+                    echo "Plik został pomyślnie przesłany i zapisany w: " . $target_file;
+                } else {
+                    echo "Wystąpił problem podczas przesyłania pliku.";
                 }
             }
         }
     }
-}
-?></html>
+} ?>
