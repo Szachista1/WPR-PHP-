31b536ecc5b73ddc16e6f374fd15d401997e9a52
DESKTOP-TG98GK0\igwar <s26986@edu.pjwstk.pl>
2023-06-21 08:37:51 +0200

    poikl

--- projkt/wpis.php
+++ projkt/wpis.php
@@ -4,4 +4,10 @@ $_SESSION['wpis']=$_POST['wpis'];
 $_SESSION['temat']=$_POST['temat'];
 echo $_SESSION['wpis']."<br>";
 echo $_SESSION['temat']."<br>".$_SESSION['psedonim'];
-?>
+?><HTML>
+<form action="komenatrz_do_zalogowanego.php" method="post">
+komentarz:    <input type="text" name="tresc"><br>
+psełdonim:    <input type="text" name="psedonim1"><br><input type="submit">
+
+</form>
+</HTML>
